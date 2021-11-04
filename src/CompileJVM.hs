36c903e97b175d36e73bdb84c999f9585820e946
cezarybednarz <cezary.bednarz@gmail.com>
2021-11-04 01:24:18 +0100

    Add JVM compiler without transpiling

--- src/CompileJVM.hs
+++ src/CompileJVM.hs
@@ -26,6 +26,11 @@ data JVMStmts = JVMStmts { stackCount :: Integer,
                            vars :: Map.Map String Reg
                          } deriving Show
 
+addJVMStmt :: JVMStmt -> JVMStmts -> JVMStmts
+addJVMStmt jvmStmt jvmStmts = 
+  JVMStmts { stackCount = stackCount jvmStmts, 
+             stmts = stmts jvmStmts ++ [jvmStmt],
+             vars = vars jvmStmts }
 
 -- insert element to map and add Store to consume top of the stack into register --
 setVar :: String -> JVMStmts -> JVMStmts 
@@ -54,10 +59,41 @@ printVal jvmStmts =
              vars = vars jvmStmts
            }
 
+addStackCount :: JVMStmts -> JVMStmts
+addStackCount jvmStmts = 
+  JVMStmts { stackCount = stackCount jvmStmts + 1,
+             stmts = stmts jvmStmts,
+             vars = vars jvmStmts
+           }
+
+combineJVMStmts :: JVMStmts -> JVMStmts -> JVMStmts
+combineJVMStmts jvmStmts1 jvmStmts2 = do
+  JVMStmts { stackCount = max (stackCount jvmStmts1) (stackCount jvmStmts2),
+             stmts = stmts jvmStmts1 ++ stmts jvmStmts2,
+             vars = vars jvmStmts1
+           }
+
+arithmExprToJVMStmts :: Op -> Exp -> Exp -> JVMStmts -> JVMStmts
+arithmExprToJVMStmts op expr1 expr2 jvmStmts = do
+  let jvmStmts1 = exprToJVMStmts expr1 jvmStmts
+  let jvmStmts2 = exprToJVMStmts expr2 jvmStmts
+  let jvmStmts3 = 
+       if stackCount jvmStmts1 >= stackCount jvmStmts2 then
+         combineJVMStmts jvmStmts1 (addStackCount jvmStmts2)
+       else 
+         addJVMStmt Swap (combineJVMStmts jvmStmts2 (addStackCount jvmStmts1))
+  let jvmStmts4 = addJVMStmt (Arithm op) jvmStmts3
+  combineJVMStmts jvmStmts jvmStmts4
+    
 exprToJVMStmts :: Exp -> JVMStmts -> JVMStmts
-exprToJVMStmts expr jvmStmts = 
-  jvmStmts -- todo usunac to 
-  -- todo skompilować przed pisaniem tej funkcji
+exprToJVMStmts (ExpLit val) jvmStmts = 
+  addJVMStmt (Const (Val val)) jvmStmts
+exprToJVMStmts (ExpVar (Ident ident)) jvmStmts = 
+  addJVMStmt (Load $ getVarReg ident jvmStmts) jvmStmts
+exprToJVMStmts (ExpAdd x y) jvmStmts = arithmExprToJVMStmts Add x y jvmStmts
+exprToJVMStmts (ExpSub x y) jvmStmts = arithmExprToJVMStmts Sub x y jvmStmts
+exprToJVMStmts (ExpMul x y) jvmStmts = arithmExprToJVMStmts Mul x y jvmStmts
+exprToJVMStmts (ExpDiv x y) jvmStmts = arithmExprToJVMStmts Div x y jvmStmts
 
 stmtToJVMStmts :: Stmt -> JVMStmts -> JVMStmts 
 stmtToJVMStmts (SAss (Ident ident) expr) jvmStmts = do
